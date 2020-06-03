273864dfd526b4e540b6cc5e6a540e86c1e52e16
Liquor030 <mrhuangbh@gmail.com>
2020-06-03 20:09:34 +0800

    JD_Bomb: 增加21点场

--- JD/JD_Bomb/jd_bomb.py
+++ JD/JD_Bomb/jd_bomb.py
@@ -19,7 +19,7 @@ def start():
     bomb_body = 'functionId=cakebaker_pk_getCakeBomb&body={}&client=wh5&clientVersion=1.0.0'
     d_time = datetime.datetime.strptime(datetime.datetime.now().strftime("%Y-%m-%d%H")+':59:58', '%Y-%m-%d%H:%M:%S')
     d_time1 = d_time+datetime.timedelta(seconds=4)
-    if d_time1.hour == 2 or d_time1.hour == 4 or d_time1.hour == 10 or d_time1.hour == 12:
+    if d_time1.hour == 2 or d_time1.hour == 4 or d_time1.hour == 10 or d_time1.hour == 12 or d_time1.hour == 13:
         while datetime.datetime.now() < d_time:
             time.sleep(1)
             logging.warning(datetime.datetime.now())
@@ -36,7 +36,7 @@ def start():
                 break
     else:
         logging.warning(datetime.datetime.now())
-        logging.warning('非活动时间(10/12/18/20)！')
+        logging.warning('非活动时间(10/12/18/20/21)！')
 
 def main_handler(event, context):
     return start()
