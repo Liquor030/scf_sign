f5d823c1c66c72f0a7c9835920e158757148e9c8
Liquor030 <mrhuangbh@gmail.com>
2020-06-04 20:57:46 +0800

    增加战况通知; 调整为每个整点执行

--- JD/JD_Bomb/jd_bomb.py
+++ JD/JD_Bomb/jd_bomb.py
@@ -19,7 +19,7 @@ def start():
     bomb_body = 'functionId=cakebaker_pk_getCakeBomb&body={}&client=wh5&clientVersion=1.0.0'
     d_time = datetime.datetime.strptime(datetime.datetime.now().strftime("%Y-%m-%d%H")+':59:58', '%Y-%m-%d%H:%M:%S')
     d_time1 = d_time+datetime.timedelta(seconds=4)
-    if d_time1.hour == 2 or d_time1.hour == 4 or d_time1.hour == 8 or d_time1.hour == 10 or d_time1.hour == 12 or d_time1.hour == 13:
+    if datetime.datetime.now().minute == 59:
         while datetime.datetime.now() < d_time:
             time.sleep(1)
             logging.warning(datetime.datetime.now())
@@ -31,12 +31,14 @@ def start():
             if '成功' in bomb:
                 s = json.loads(bomb)
                 msg = urllib.quote(str(s["data"]["result"]["tip"]))
-                requests.get('https://api.day.app/@@@@@@@@@/JD_Bomb/'+msg)
+                groupLevel = urllib.quote(str(s["data"]["result"]["groupLevel"]))
+                opponentLevel = urllib.quote(str(s["data"]["result"]["opponentLevel"]))
+                requests.get('https://api.day.app/@@@@@@@@@/JD_Bomb/'+msg+'%0a%e6%88%98%e5%86%b5%ef%bc%9a'+groupLevel+'+VS+'+opponentLevel)
                 logging.warning('成功')
                 break
     else:
         logging.warning(datetime.datetime.now())
-        logging.warning('非活动时间(10/12/16/18/20/21)！')
+        logging.warning('非活动时间！')
 
 def main_handler(event, context):
     return start()
