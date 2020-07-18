f622e5235243d892f44fa5653326cb0e0a8a2f94
Liquor030 <mrhuangbh@gmail.com>
2020-07-19 00:39:00 +0800

    Fix Bug.

--- QD_refreshUpdateinfo/index.js
+++ QD_refreshUpdateinfo/index.js
@@ -1,41 +1,43 @@
 'use strict';
+const fetch = require('node-fetch');
+const tencentcloud = require("tencentcloud-sdk-nodejs");
+const ScfClient = tencentcloud.scf.v20180416.Client;
+const models = tencentcloud.scf.v20180416.Models;
+const Credential = tencentcloud.common.Credential;
+const ClientProfile = tencentcloud.common.ClientProfile;
+const HttpProfile = tencentcloud.common.HttpProfile;
+
+//////////// 配置 ////////////
+const ids = [1018027842, 1016572786]; // 填入需要获取更新通知的起点书籍id
+const Bark_Key = "#########"; // 填入你自己的Bark_Key
+const SecretId = "@@@@@@@@@"; // 填入腾讯云API密钥
+const SecretKey = "$$$$$$$$$"; // https://console.cloud.tencent.com/cam/capi
+const FunctionName = "&&&&&&&&&"; // 创建的函数名称
+const Region = "¥¥¥¥¥¥¥¥¥"; // 地域配置为创建函数时选择的地域，可对照README的地域对应表进行填写
+/////////////////////////////
+
 exports.main_handler = async (event, context, callback) => {
-
-    //////////// 配置 ////////////
-    const ids = [1018027842, 1016572786]; // 填入需要获取更新通知的起点书籍id
-    const Bark_Key = "#########"; // 填入你自己的Bark_Key
-    const SecretId = "@@@@@@@@@"; // 填入腾讯云API密钥
-    const SecretKey = "$$$$$$$$$"; // https://console.cloud.tencent.com/cam/capi
-    const FunctionName = "&&&&&&&&&"; // 创建的函数名称
-    const Region = "ap-shanghai"; // 地域配置为创建函数时所选择的地域，默认上海
-    /////////////////////////////
-
-    const fetch = require('node-fetch');
-    const tencentcloud = require("tencentcloud-sdk-nodejs");
-    const ScfClient = tencentcloud.scf.v20180416.Client;
-    const models = tencentcloud.scf.v20180416.Models;
-    const Credential = tencentcloud.common.Credential;
-    const ClientProfile = tencentcloud.common.ClientProfile;
-    const HttpProfile = tencentcloud.common.HttpProfile;
-
     async function refreshUpdateinfo(id) {
         await fetch('http://druid.if.qidian.com/Atom.axd/Api/Book/GetChapterList?BookId=' + id + '&timeStamp=253402185599000')
             .then(res => res.json())
             .then(json => {
                 if (json.Result == 0) {
                     let obj = json.Data.LastVipUpdateChapterId ? 'LastVip' : 'Last';
-                    console.log('\n' + json.Data.BookName + '_更新时间: ' + json.Data[obj + 'ChapterUpdateTime']);
+                    console.log('\n' + id + '_' + json.Data.BookName + '_更新时间: ' + json.Data[obj + 'ChapterUpdateTime']);
                     if (process.env['bid_' + id] == undefined) {
                         console.log('\n' + json.Data.BookName + '_更新章节: ' + json.Data[obj + 'UpdateChapterName']);
                         Bark(json.Data.BookName, json.Data.Author, json.Data[obj + 'UpdateChapterName'], Bark_Key);
                         refreshVariables(id, json.Data[obj + 'UpdateChapterId']);
+                        flag = 1;
                     } else {
                         if (process.env['bid_' + id] == json.Data[obj + 'UpdateChapterId']) {
                             console.log('\n' + json.Data.BookName + ': 暂无更新');
+                            refreshVariables(id, json.Data[obj + 'UpdateChapterId']);
                         } else {
                             console.log('\n' + json.Data.BookName + '_更新章节: ' + json.Data[obj + 'UpdateChapterName']);
                             Bark(json.Data.BookName, json.Data.Author, json.Data[obj + 'UpdateChapterName'], Bark_Key);
                             refreshVariables(id, json.Data[obj + 'UpdateChapterId']);
+                            flag = 1;
                         }
                     }
                 } else {
@@ -86,9 +88,14 @@ exports.main_handler = async (event, context, callback) => {
 
     async function main() {
         await _refreshUpdateinfo();
-        refreshEnvironment();
+        if (flag) {
+            refreshEnvironment();
+        } else {
+            console.log('\n' + '所有书籍均无更新！')
+        }
     }
 
+    var flag = 0;
     var BookInfo = JSON.parse('{\"FunctionName\":\"' + FunctionName + '\",\"Environment\":{\"Variables\":[]}}')
     main();
 
