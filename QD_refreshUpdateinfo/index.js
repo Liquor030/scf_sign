a8d7ba871ba49590aa917c4018150dcaaa4008eb
Liquor030 <mrhuangbh@gmail.com>
2020-07-18 09:11:11 +0800

    调整 检测更新的逻辑

--- QD_refreshUpdateinfo/index.js
+++ QD_refreshUpdateinfo/index.js
@@ -1,24 +1,31 @@
 'use strict';
 exports.main_handler = async (event, context, callback) => {
     const fetch = require('node-fetch');
+
+    //////////// 配置 ////////////
     const rate = 60; // 注意: 更改此项需要配合正确的定时触发使用，默认60秒。
     const ids = [1018027842]; // 填入需要获取更新通知的起点书籍id
     const Bark_Key = "#########"; // 填入你自己的Bark_Key
+    /////////////////////////////
 
     function refreshUpdateinfo(id) {
-        fetch('http://druid.if.qidian.com/Atom.axd/Api/Book/GetChapterList?BookId=' + id + '&timeStamp=' + time)
+        fetch('http://druid.if.qidian.com/Atom.axd/Api/Book/GetChapterList?BookId=' + id + '&timeStamp=' + now)
             .then(res => res.json())
             .then(json => {
-                if (json.Result == -101) {
-                    console.log('id_' + id + ':' + json.Message);
-                } else {
-                    let len = json.Data.Chapters.length;
-                    if (len == 0) {
-                        console.log(json.Data.BookName + ': 暂无更新');
-                    } else {
-                        console.log(json.Data.BookName + ': ' + json.Data.Chapters[len - 1].n);
-                        Push(json.Data.BookName, json.Data.Author, json.Data.Chapters[len - 1].n, Bark_Key);
+                if (json.Result == 0) {
+                    let obj = json.Data.LastVipChapterUpdateTime ? 'LastVip' : 'Last';
+                    let DiffTime = now - json.Data[obj+'ChapterUpdateTime']
+                    if (DiffTime <= rate && DiffTime > 0)
+                    {
+                        console.log(json.Data.BookName + ': ' + json.Data[obj+'UpdateChapterName']);
+                        Push(json.Data.BookName, json.Data.Author, json.Data[obj+'UpdateChapterName'], Bark_Key);
                     }
+                    else
+                    {
+                        console.log(json.Data.BookName + ': 暂无更新');
+                    }
+                } else {
+                    console.log('id_' + id + ':' + json.Message);
                 }
             });
     }
@@ -29,9 +36,9 @@ exports.main_handler = async (event, context, callback) => {
             .then(json => console.log(json.message));
     }
 
-    var time = new Date().getTime();
-    time -= (time % (rate * 1000) + rate * 1000);
-    console.log("refresh_timeStamp: " + time);
+    var now = new Date().getTime();
+    now -= (now % (rate * 1000));
+    console.log("refresh_timeStamp: " + now);
 
     for (let i = 0; i < ids.length; i++) {
         refreshUpdateinfo(ids[i]);
